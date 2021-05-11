ba843021ee2696efcab204ebdac9a860241b6bed
minnuss <ponjevic.goran@hotmail.com>
2021-05-12 00:20:24 +0200

    added input for mobile devices

--- js/main.js
+++ js/main.js
@@ -105,6 +105,31 @@ window.addEventListener('keydown', (e) => {
     hiddenInput.focus()
 })
 
+hiddenInput.addEventListener('keydown', (e) => {
+    // check only for letters
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+        const letter = e.key
+
+        if (selectedWord.includes(letter)) {
+            if (!correctLetters.includes(letter)) {
+                correctLetters.push(letter)
+                displayWord()
+            } else {
+                showNotification()
+            }
+        } else {
+            if (!wrongLetters.includes(letter)) {
+                wrongLetters.push(letter)
+
+                updateWrongLettersEl()
+            } else {
+                showNotification()
+            }
+        }
+    }
+    hiddenInput.focus()
+})
+
 // PLAY AGAIN BUTTON
 btnPlay.addEventListener('click', () => {
     // empty arrays
